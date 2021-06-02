402961ba21c915f71334ac634a9eff42867fba7b
AutumnFrog <83947355+AutumnFrog@users.noreply.github.com>
2021-06-03 09:41:54 +1200

    Added an end turn function
    End turn function handles villagers professions at the end of the turn

--- villagers.py
+++ villagers.py
@@ -2,16 +2,31 @@
 # Villager Game
 # Villager Module
 # Written by Madeline Autumn
-# Last modified on 31/05/21
+# Last modified on 03/06/21
 #
 
 ### Imports and Varibles ###
-
+import config
+import random
 
 ### Villager class ###
 
 class Villager:
 
-    def __init__(self, name):
+    def __init__(self, name, profession):
 
         self.name = name
+        self.profession = profession
+
+        # Villagers initial stats
+        self.hunger = 0
+        self.health_max = 12
+        self.health = self.health_max
+
+    def end_turn(self):
+        
+        # Calculate stat changes
+        self.hunger += random.randint(1,3)
+        
+        # Run action
+        self.profession.action(self)
