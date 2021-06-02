773ce7da3dea653f20499122ef6f1df04f5792fd
AutumnFrog <83947355+AutumnFrog@users.noreply.github.com>
2021-06-03 09:48:48 +1200

    Moved the gui class to a separate file

--- main.py
+++ main.py
@@ -6,36 +6,15 @@
 #
 
 ### Importants and Varibles ###
-import tkinter as tk
+import config
+import game_app
 import villagers, professions
 
-### Game Application Class ###
-
-class GameApp:
-
-    def __init__(self):
-        
-        # Initialize the window
-        self.root = tk.Tk()
-        self.root.title('Villager game')
-        self.root.resizable(width=0, height=0)
-
-        # Create the two frames for the gui
-        self.left_frame = tk.LabelFrame(self.root, text='')
-        self.right_frame = tk.LabelFrame(self.root, text='')
-        self.left_frame.grid(row=0, column= 0, padx= 4, pady= 4)
-        self.right_frame.grid(row=0, column= 1, padx= 4, pady= 4)
-
-        # Create the frames for the village
-        self.village_frame = tk.LabelFrame(self.left_frame, text='Village', width= 48)
-        self.village_frame.grid(row=0, column= 0, padx= 2, pady= 2)
-
-        # Create the frame for the statistics
-        self.stats_frame = tk.LabelFrame(self.left_frame, text='Statistics')
-        self.stats_frame.grid(row=1, column= 0, padx= 2, pady= 2)
+# Initialize the globals
+config.init()
 
 ### Main Game Loop ###
 
 if __name__ == '__main__':
-    main_app = GameApp()
+    main_app = game_app.GameApp()
     main_app.root.mainloop()
