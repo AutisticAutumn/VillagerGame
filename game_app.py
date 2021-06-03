0172c6aec7c13c12ad1cb53f8992e5945ecb7dbf
AutumnFrog <83947355+AutumnFrog@users.noreply.github.com>
2021-06-04 11:15:27 +1200

    Added turns
    There is now an update gui function that updates all the gui.
    The feeding and hunger for villagers is now dealt with within the gui function at the end of the turn

--- game_app.py
+++ game_app.py
@@ -9,6 +9,7 @@
 import tkinter as tk
 from villagers import Villager
 import config
+import random
 
 ### Classes ###
 
@@ -47,19 +48,20 @@ class VillagerFrame:
         self.ascii_art = tk.Label(self.ascii_frame, text='', relief=tk.GROOVE, borderwidth=2, width=8)
         self.ascii_art.grid(row=0, column=0, padx=2, pady=2, sticky=tk.EW)
 
-        self.ascii_name = tk.Label(self.ascii_frame, text=self.villager.name)
+        self.ascii_name_var = tk.StringVar()
+        self.ascii_name = tk.Label(self.ascii_frame, textvariable=self.ascii_name_var)
         self.ascii_name.grid(row=1, column=0, padx=2, pady=2, sticky=tk.EW)
 
         self.kill_button = tk.Button(self.ascii_frame, text='Kill', width=8)
         self.kill_button.grid(row=2, column=0, padx=2, pady=2)
 
         # Stats frame widgets #
-        self.title = f'{self.villager.name} the {self.villager.profession.name}'
-        self.name_frame = tk.Label(self.stats_frame, text=self.title, width=30)
+        self.title = tk.StringVar()
+        self.name_frame = tk.Label(self.stats_frame, textvariable=self.title, width=30)
         self.name_frame.grid(row=0, column=0, padx=2, pady=6, sticky=tk.NSEW)
 
-        self.stats = f'Health: {self.villager.health}        Hunger: {self.villager.hunger}'
-        self.stats_frame = tk.Label(self.stats_frame, text=self.stats)
+        self.stats = tk.StringVar()
+        self.stats_frame = tk.Label(self.stats_frame, textvariable=self.stats)
         self.stats_frame.grid(row=1, column=0, padx=2, pady=6, sticky=tk.NSEW)
 
         # Professions frame widgets #
@@ -70,7 +72,6 @@ class VillagerFrame:
         self.professions_menu.grid(row=2, column=0, padx=2, pady=6, sticky=tk.NSEW)
 
         # Button Frame widgets #
-
         self.food_label = tk.Label(self.button_frame, text='Food Priority:')
         self.food_label.grid(row=0, column=0, padx=2, pady=4, sticky=tk.NSEW)
         self.food_menu_var = tk.StringVar()
@@ -80,7 +81,14 @@ class VillagerFrame:
             tk.Radiobutton(self.button_frame,
                            text=value, value=value,
                            variable=self.food_menu_var,
-                           indicator = 0, width=6).grid(padx=2, pady=2)
+                           indicator = 0, width=6,).grid(padx=2, pady=2)
+    
+    def update_stats(self):
+        '''Updates the onscreen stats and data for the villager'''
+
+        self.ascii_name_var.set(self.villager.name)
+        self.title.set(f'{self.villager.name} the {self.villager.profession.name}')
+        self.stats.set(f'Health: {self.villager.health}        Hunger: {self.villager.hunger}')
 
 ## Game Application Class ##
 
@@ -114,12 +122,10 @@ class GameApp:
 
         # Create the labels for the statistics
         self.food_stat_var = tk.StringVar()
-        self.food_stat_var.set(f'Total Food: {config.food}')
         self.food_stat_label = tk.Label(self.stats_frame, textvariable=self.food_stat_var)
         self.food_stat_label.grid(row=1, column=0, padx=8, pady=2, sticky=tk.NSEW)
 
         self.wood_stat_var = tk.StringVar()
-        self.wood_stat_var.set(f'Total Wood: {config.wood}')
         self.wood_stat_label = tk.Label(self.stats_frame, textvariable=self.wood_stat_var)
         self.wood_stat_label.grid(row=1, column=1, padx=8, pady=2, sticky=tk.NSEW)
 
@@ -144,9 +150,7 @@ class GameApp:
         self.mod_scrollbar.grid(row=0, column=1, sticky=tk.NSEW)
 
         # Add the villager modification frames
-        self.villager_frames = []
-        for villager in config.villagers:
-            self.create_villager_frame(villager)
+        self.create_villager_frames()
 
         # Button to end turn
         self.end_turn_button = tk.Button(self.center_frame, 
@@ -168,15 +172,79 @@ class GameApp:
         self.log_text.config(yscrollcommand=self.log_scrollbar.set)
         self.log_scrollbar.config(command=self.log_text.yview)
 
-    def create_villager_frame(self, villager):
+        self.update_stats()
+
+    def add_villager_frame(self, villager):
         '''Creates the frame onscreen to display the villager'''
 
         villager_frame = VillagerFrame(self, villager, len(self.villager_frames))
         self.villager_frames.append(villager_frame)
         villager_frame.create_widgets()
 
+    def create_villager_frames(self):
+        '''Runs through all villagers and frames them all'''
+
+        self.villager_frames = []
+        for villager in config.villagers:
+            self.add_villager_frame(villager)
+
+    def update_stats(self):
+        '''Updates the onscreen stats'''
+
+        self.food_stat_var.set(f'Total Food: {config.food}')
+        self.wood_stat_var.set(f'Total Wood: {config.wood}')
+
+        for villager_frame in self.villager_frames:
+            villager_frame.update_stats()
+
+    def feed_villagers(self, priotity):
+        '''Runs through the list of villagers and uses the frame to
+            check for the right priotity'''
+        
+        # Create a list of the villagers that match the priotity
+        feeding = []
+        for villager_frame in self.villager_frames:
+            if villager_frame.food_menu_var.get() == priotity:
+                feeding.append(villager_frame.villager)
+
+        # Shuffle the list and feed the apprioprite villagers
+        if feeding != []:
+            random.shuffle(feeding)
+            for villager in feeding:
+                # Only caluate food if needed
+                if villager.hunger > 0:
+                    if config.food > 0:
+                        init_food = config.food
+                        config.food -= villager.hunger
+                        villager.hunger = 0
+                        if config.food < 0:
+                            # Add back food and hunger so that food > 0
+                            villager.hunger += config.food*-1
+                            config.food += config.food*-1
+                        food_consumed = init_food - config.food
+                        # Add result to log
+                        villager.log.append(f'{villager.name} has consumed {food_consumed} food')
+                    else:
+                        # Add result to log
+                        villager.log.append(f'There is no food for {villager.name} to consume')
+                        # Add hunger if no food was consumed
+                        villager.hunger += random.randint(config.hunger_range[0],
+                                                          config.hunger_range[1])
+                else:
+                    # Add hunger if no food was consumed
+                    villager.hunger += random.randint(config.hunger_range[0],
+                                                      config.hunger_range[1])
+
     def end_turn(self):
         '''Run end of turn functions'''
+
+        # Run through and feed all the villagers in the right order
+        for priority in config.food_priority_values:
+            self.feed_villagers(priority)
+
+        # Run through all the villagers and run their actions
+        for villager in config.villagers:
+            villager.end_turn()
 
         # Updates onscreen logs
         self.log_text.config(state=tk.NORMAL)
@@ -195,8 +263,15 @@ class GameApp:
         # Add the turns log to the main log and reset the turn log
         for line in config.turn_log:
             config.log.append(line)
-        config.log = []
+        config.turn_log = []
+
+        # Run the beginning of turn functions just before the next turn begins
+        for villager in config.villagers:
+            villager.begin_turn()
 
         # Update turn counter and add to the logs
         config.turn += 1
         config.log.append(f'\nTurn {config.turn}')
+
+        # Update the gui
+        self.update_stats()
