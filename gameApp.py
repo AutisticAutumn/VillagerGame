a8017fc5c746a3c3c5ace3afc8ecce93ea3839ce
AutumnFrog <83947355+AutumnFrog@users.noreply.github.com>
2021-06-05 21:27:40 +1200

    Fixed capitalization issue

--- gameApp.py
+++ gameApp.py
@@ -2,7 +2,7 @@
 # Villager Game
 # Main Game Application Module
 # Written by Madeline Autumn
-# Last modified on 04/06/21
+# Last modified on 05/06/21
 #
 
 ### Importants and Varibles ###
@@ -34,7 +34,7 @@ class GameApp:
         # Create the frames for the village
         self.village_frame = tk.Frame(self.left_frame)
         self.village_frame.grid(row=0, column=0, padx=2, pady=2, sticky= tk.NSEW)
-        self.map = mapUI.map(self, self.village_frame)
+        self.map = mapUI.Map(self, self.village_frame)
         
         self.stats_box = tk.Text(self.left_frame,
                                  width=48, 
