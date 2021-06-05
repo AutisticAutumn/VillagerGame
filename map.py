aefa70376c1bc7ef120af6bed1b7f1395f3076bd
AutumnFrog <83947355+AutumnFrog@users.noreply.github.com>
2021-06-05 22:28:15 +1200

    Building will now automatically cost materials

--- map.py
+++ map.py
@@ -17,22 +17,38 @@ class Map:
 
         self.frame = frame
     
-    def build_building(self, key):
+    def build_building(self, key, pay=True):
         '''Creates a building on the map'''
 
+        building = config.get_building(key)
+
+        # Get the building position
         posy = 6
         posx = 13
 
+        # Make payments for building
+        if pay:
+            # Check food
+            if building.cost['food'] <= config.food:
+                config.food -= building.cost['food']
+            else:
+                return False
+            
+            # Check wood
+            if building.cost['wood'] <= config.wood:
+                config.wood -= building.cost['wood']
+            else:
+                return False
+
         # Add each segement seperately to the main map dictionary
-        for x in range(config.get_building(key).size[0]):
-            for y in range(config.get_building(key).size[1]):
-
+        for x in range(building.size[0]):
+            for y in range(building.size[1]):
+                
+                # Get unique building object and data 
                 building = config.get_building(key)
-
                 segment = x + (y*building.size[0])
-
                 pos_key = f'({posy+y}:{posx+x})'
 
+                # Add building to map
                 config.map[pos_key] = building
                 config.map[pos_key].pos = segment
-                print(config.map[pos_key])
