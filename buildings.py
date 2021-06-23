9ca6092d02e8ab9b32ed9241740076eddaee24e8
AutumnFrog <83947355+AutumnFrog@users.noreply.github.com>
2021-06-23 14:39:05 +1200

    Added farm building

--- buildings.py
+++ buildings.py
@@ -105,3 +105,35 @@ class WoodenStatue(Building):
         # Run through all villagers and add to their happiness by 1
         for villager in config.villagers:
             villager.gain_happiness(1, 1)
+
+class Farm(Building):
+    '''A farm the provides a space for farmers to work
+        hold one farmer job'''
+
+    def __init__(self, pos_x=None, pos_y=None):
+
+        self.name = 'Farm'
+        self.description = 'Provides food for the village'
+
+        self.size = (3,2)
+        self.texture = '''
+≈≈≈
+≈≈≈'''
+        # Produce a randomized colourmap for each object
+        self.colour_map = []
+        self.colours = ['sienna3', 'spring green2']
+
+        for i in range(self.size[0]*self.size[0]):
+            self.colour_map.append(random.randint(0, len(self.colours)-1))
+        
+        
+
+        self.cost = {'food': 5,
+                     'wood': 0}
+    
+    def on_creation(self):
+        '''Runs functions for when the building is built'''
+        
+        # Run through all villagers and add to their happiness by 1
+        for villager in config.villagers:
+            villager.gain_happiness(1, 1)
