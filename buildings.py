fbab61cfda2382e56efa9c875e210aebe0babe4a
AutumnFrog <83947355+AutumnFrog@users.noreply.github.com>
2021-06-24 14:43:56 +1200

    Farm now starts empty

--- buildings.py
+++ buildings.py
@@ -1,202 +1,177 @@
 #
 # Villager Game
-# Buildings Module
+# Professions Module
 # Written by Madeline Autumn
 #
 
 ### Imports and variables ###
-import random
 import config
+import map
+import random
 
-### Buildings ###
+### Professions ###
 
-class Building:
-    '''Main building class'''
+class Profession:
+    '''Main profession class that deals with globals'''
 
     def __init__(self):
 
-        self.pos_x = None
-        self.pos_y = None
+        self.building = None
 
-        self.profession = None
-        self.worker = None
-
-    def get_texture(self, pos):
-        '''returns the texture for the building'''
-
-        texture = self.texture.replace('\n','')[pos]
-        colour = self.colours[self.colour_map[pos]]
-        
-        return (texture, colour)
-
-    def update_texture_map(self):
-        x0 = self.pos_x
-        x1 = self.pos_x + self.size[0]
-
-        y0 = self.pos_y
-        y1 = self.pos_y + self.size[1]
-
-        # Run through the complete building
-        for y in range(y0, y1):
-            for x in range(x0, x1):
-                    
-                # Get position and texture
-                pos = (x-x0)+((y-y0)*self.size[0])
-                texture = self.get_texture(pos)
-                
-                # Update the texture map
-                pos = x + ((y-1)*config.map.width)
-                config.map.texture_map[pos] = texture
-
-    def on_creation(self):
-        '''Runs functions for when the building is built'''
+    def action(self, villager):
         pass
-
-    def begin_turn(self):
-        '''Run any start of turn functions for the building'''
-        pass
-
-class Grass:
-    '''Default grass. Does nothing'''
-
-    def __init__(self):
-        
-        self.name = 'Grass'
-        self.description = 'Grass'
-        self.texture = '''  '".,  '''
-        self.colours = ['green']
-
-    def get_texture(self, randkey):
-        '''returns the texture for the building'''
-
-        # Make sure texture is psudo-random 
-        random.seed(config.grass_seed + randkey)
-        texture = random.choice(self.texture)
-        random.seed(config.seed + config.turn)
-        
-        return (texture, self.colours[0])
-
-class WoodenHut(Building):
-    '''Simple build that holds two villagers'''
-
-    def __init__(self):
-
-        # Get inherited data
-        Building.__init__(self)
-
-        # Get building data
-        self.name = 'Wooden Hut'
-        self.description = 'A simple wooden hut'
-
-        self.size = (4,3)
-        self.texture = '''
-┌──┐
-│##│
-└──┘'''
-        self.colour_map = (0,0,0,0,
-                           0,1,1,0,
-                           0,0,0,0)
-        
-        self.colours = ['chocolate3', 'brown4']
-
-        self.cost = {'food': 0,
-                     'wood': 10}
-
-    def on_creation(self):
-        '''Runs functions for when the building is built'''
-        
-        # Increase the max number of villagers
-        config.max_villagers += 1 
-
-class WoodenStatue(Building):
-    '''Simple statue that provides an instant boost to happiness
-        slightly increases chance of new villagers'''
-
-    def __init__(self, pos_x=None, pos_y=None):
-        
-        # Get inherited data
-        Building.__init__(self)
-
-        # Get building data
-        self.name = 'Wooden Statue'
-        self.description = 'A simple wooden statue'
-
-        self.size = (3,3)
-        self.texture = '''
- ± 
-┐┼┌
-≡≡≡'''
-        self.colour_map = (0,2,0,
-                           0,0,0,
-                           1,1,1)
-        
-        self.colours = ['chocolate3', 'brown4', 'navajo white']
-
-        self.cost = {'food': 0,
-                     'wood': 6}
     
-    def on_creation(self):
-        '''Runs functions for when the building is built'''
-        
-        # Run through all villagers and add to their happiness by 1
-        for villager in config.villagers:
-            villager.gain_happiness(1, 1)
+    def turn_action(self, villager):
+        '''Opens the popout for the placement'''
 
-class Farm(Building):
-    '''A farm the provides a space for farmers to work
-        hold one farmer job'''
+        building = config.get_building(self.buildings[0])
+
+        config.map.popout.villager = villager
+        config.map.popout.building = building
+        config.map.popout.create_toplevel()
+
+    def turn_action_popout_closed(self, villager, building, pos):
+        '''Runs the functions for when the popout closes'''
+
+        # Set the action for the villager
+        villager.turn_action = (config.map.build_building, building, pos)
+
+        # Return output to logs
+        response = config.get_response('build_turn_action')
+        response = response.format(villager.name, building.name)
+        villager.append_villager_log(response, 'lime')
+    
+
+class Unemployed(Profession):
+    '''Unemployed villagers provide no materials or bonuses but can build buildings'''
+
+    def __init__(self):
+
+        # Inherit data
+        Profession.__init__(self)
+
+        # Villager info
+        self.name = 'Unemployed'
+        self.description = ''
+
+class Farmer(Profession):
+    '''The farmer provides foods for the village at Farms'''
+
+    def __init__(self):
+
+        # Inherit data
+        Profession.__init__(self)
+
+        # Villager info
+        self.name = 'Farmer'
+        self.description = 'Provides 2-4 food each turn'
+        self.building = 'Farm'
+
+        # Farm construction info
+        self.action_text = 'Construct farm'
+        self.buildings = ['Farm']
+
+    def action(self, villager):
+        '''Collect food'''
+        
+        # Check if a build is going to be build as first priority
+        if villager.turn_action != None:
+
+            # if building cannot be build return error
+            build = villager.turn_action[0](
+                                            villager.turn_action[1],
+                                            villager.turn_action[2][0],
+                                            villager.turn_action[2][1]
+                                            )
+
+            if build:
+                
+                # Run on creation functions for building
+                villager.turn_action[1].on_creation()
+
+                response = config.get_response('build_action_succeed')
+                return (response.format(villager.name, villager.turn_action[1].name), 'cyan')
+
+            else:
+                response = config.get_response('farmer_action_build_fail')
+                return (response.format(villager.name, villager.turn_action[1].name), 'red')
+
+        else:
+            # Check if the farm can work at a farm
+            if villager.work_building != None:
+
+                # Get food from farm
+                food = villager.work_building.food
+                config.food += food
+
+                # Add food to the farm
+                food_produced = random.randint(1,3)
+                villager.work_building.reset_texture(food_produced)
+                
+                # Return output
+                response = config.get_response('farmer_action')
+                return (response.format(villager.name, food), 'lime')
+
+class Feller(Profession):
+    '''The Feller provides Wood for the village'''
 
     def __init__(self):
         
-        # Get inherited data
-        Building.__init__(self)
+        # Inherit data
+        Profession.__init__(self)
 
-        # Get building data
-        self.name = 'Farm'
-        self.description = 'Provides food for the village'
-        self.food = 0
+        # Villager info
+        self.name = 'Feller'
+        self.description = 'Provides 2-3 wood each turn'
 
-        self.size = (3,2)
+    def action(self, villager):
+        '''Collect Wood'''
 
-        # Produce a randomized colourmap for each object
-        self.colours = ['sienna1', 'chocolate3', 'indianred3', 'goldenrod', 'forest green']
+        wood_produced = random.randint(2,3)
+        config.wood += wood_produced
 
-        self.profession = 'Farmer'
+        response = config.get_response('feller_action')
+        return (response.format(villager.name, wood_produced), 'chocolate')
 
-        # add crops to farm
-        self.reset_texture(random.randint(1,3))
-        self.cost = {'food': 5,
-                     'wood': 0}
+class Carpenter(Profession):
+    '''The carpenter builds building with wood'''
 
-    def reset_texture(self, crops):
-        '''Reset the texture to include new crops'''
+    def __init__(self):
+        
+        # Inherit data
+        Profession.__init__(self)
 
-        # Reset texture data
-        self.texture = ''
-        texture_temp = ''
-        self.colour_map = []
-        self.food = 0
+        # Villager info
+        self.name = 'Carpenter'
+        self.description = 'Constructs wooden buildings'
 
-        # Add crops and dirt
-        for i in range(crops):
-            texture_temp += '♠'
-            self.food += 1
+        self.action_text = 'Construct'
 
-        for i in range((self.size[0]*self.size[1])-crops):
-            texture_temp += '≈'
+        # List of available buildings
+        self.buildings = ('Wooden Hut',
+                          'Wooden Statue')
 
-        # Shuffle the string
-        self.texture = ''.join(random.sample(texture_temp ,len(texture_temp)))
+    def action(self, villager):
+        '''Build a building if the action was selected'''
 
-        # Add colour to the texture
-        for texture in self.texture:
-            if texture == '♠':
-                self.colour_map.append(random.randint(2, 4))
-            else: 
-                self.colour_map.append(random.randint(0, 1))
+        # Only attempt to build if action was seleceted
+        if villager.turn_action != None:
 
-        # Attempt to update texture on the map if possible
-        try:
-            self.update_texture_map()
-        except:
-            pass
+            # if building cannot be build return error
+            build = villager.turn_action[0](
+                                            villager.turn_action[1],
+                                            villager.turn_action[2][0],
+                                            villager.turn_action[2][1]
+                                            )
+
+            if build:
+                
+                # Run on creation functions for building
+                villager.turn_action[1].on_creation()
+
+                response = config.get_response('build_action_succeed')
+                return (response.format(villager.name, villager.turn_action[1].name), 'cyan')
+
+            else:
+                response = config.get_response('carpenter_action_no_wood')
+                return (response.format(villager.name, villager.turn_action[1].name), 'red')
