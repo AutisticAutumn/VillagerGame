cde9fcaadbdce8b5a4c0d8873481a2ce01164db4
AutumnFrog <83947355+AutumnFrog@users.noreply.github.com>
2021-07-26 22:42:12 +1200

    World size can now be adjusted in the menu

--- menu.py
+++ menu.py
@@ -6,9 +6,16 @@
 
 ### Importants and Varibles ###
 import tkinter as tk
-import config
+import config, string
 
 ### Classes ###
+class Del:
+  def __init__(self, keep=string.digits):
+    self.comp = dict((ord(c),c) for c in keep)
+  def __getitem__(self, k):
+    return self.comp.get(k)
+
+DD = Del()
 
 class MenuApp():
     '''The popout for the menu'''
@@ -53,8 +60,8 @@ class MenuApp():
 
         # Initiate toplevel
         self.creation_root = tk.Toplevel()
-        self.root.title('Create Game')
-        self.root.resizable(width=0, height=0)
+        self.creation_root.title('Create Game')
+        self.creation_root.resizable(width=0, height=0)
 
         # Widgets
         self.village_name = tk.StringVar()
@@ -63,9 +70,35 @@ class MenuApp():
                                  textvariable=self.village_name)
         self.village_name.set('Unnamed Village')
         self.name_box.grid(row=0, column=0, padx=8, pady=8)
+        self.name_box.bind("<FocusOut>", self.round_data)
 
         self.settings_frame = tk.Frame(self.creation_root)
         self.settings_frame.grid(row=1, column=0, padx=8, pady=8)
+
+        self.settings_title = tk.Label(self.settings_frame, text='World Settings')
+        self.settings_title.grid(row=0, column=0, columnspan=2, pady=4)
+
+        self.width_text = tk.Label(self.settings_frame, text='Width:')
+        self.width_text.grid(row=1, column=0, sticky=tk.E)
+
+        self.width = tk.StringVar()
+        self.width_box = tk.Entry(self.settings_frame,
+                                  width=10,
+                                  textvariable=self.width)
+        self.width.set(config.map.default_width)
+        self.width_box.grid(row=1, column=1, sticky=tk.W, pady=2)
+        self.width_box.bind("<FocusOut>", self.round_data)
+
+        self.height_text = tk.Label(self.settings_frame, text='Height:')
+        self.height_text.grid(row=2, column=0, sticky=tk.E)
+
+        self.height = tk.StringVar()
+        self.height_box = tk.Entry(self.settings_frame,
+                                  width=10,
+                                  textvariable=self.height)
+        self.height.set(config.map.default_height)
+        self.height_box.grid(row=2, column=1, sticky=tk.W, pady=2)
+        self.height_box.bind("<FocusOut>", self.round_data)
 
         self.advanced_settings_button = tk.Button(self.creation_root,
                                                   text='Advanced settings',
@@ -81,11 +114,31 @@ class MenuApp():
 
         self.create_world_button.grid(row=3, column=0, padx=8, pady=8)
 
+        # Track entry boxes to keep data in range
+        self.village_name.trace("w", lambda name, index, mode: self.round_data())
+        
+    def round_data(self, button=None):
+        '''Make sure all entered data is within bounds'''
+
+        self.village_name.set(self.village_name.get()[:24])
+
+        self.width.set(self.width.get().translate(DD))
+        self.width.set(max(int(self.width.get()), config.world_size_min))
+        self.width.set(min(int(self.width.get()), config.world_size_max))
+
+        self.height.set(self.height.get().translate(DD))
+        self.height.set(max(int(self.height.get()), config.world_size_min))
+        self.height.set(min(int(self.height.get()), config.world_size_max))
+
+        config.map.width = int(self.width.get())
+        config.map.height = int(self.height.get())
+        config.map.get_world_data()
+
     def create_world(self):
         '''Close the application and begin the game'''
 
         # Adjust config variables accordingly
-        config.village_name = self.village_name.get()[:24]
+        config.village_name = self.village_name.get()
 
         # Adjust variables for main loop
         self.generate_world = True
